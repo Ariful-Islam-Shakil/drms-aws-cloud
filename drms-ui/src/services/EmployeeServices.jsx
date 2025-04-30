76b88e88fc28b02c1079ab6de16c32b4e4368d64
Ariful-Islam-Shakil <arif.cse18cuet@gmail.com>
2025-04-30 11:27:52 +0600

    Updated APi with insertion of getEmployeeById also changed api endpoints

--- drms-ui/src/services/EmployeeServices.jsx
+++ drms-ui/src/services/EmployeeServices.jsx
@@ -5,14 +5,17 @@ const API_BASE_URL = 'http://127.0.0.1:8000';
 class EmployeeService {
 
     addEmployee(name){
-        return axios.post(API_BASE_URL + '/employee', name);
+        return axios.post(API_BASE_URL + '/employee/add', name);
     }
 
     updateEmployee(u_id, updatedFields){
-      return axios.put(API_BASE_URL + '/employee/' + u_id, updatedFields);
+      return axios.put(API_BASE_URL + '/employee/update/' + u_id, updatedFields);
     }
     getEmployees(){
-      return axios.get(API_BASE_URL + '/employees');
+      return axios.get(API_BASE_URL + '/employees/getEmployees');
+    }
+    getEmployeeById(u_id){
+      return axios.get(API_BASE_URL + '/employees/getEmployee/' + u_id)
     }
 }
 
